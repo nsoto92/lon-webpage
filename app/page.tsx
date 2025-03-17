c381d1dcf4cd4e3606759c79b9ef039d4907da09
Norbert M. Soto <57424278+nsoto92@users.noreply.github.com>
2025-03-17 17:12:43 -0400

    me cago en la madre

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import { Button } from '@mantine/core';
+
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
