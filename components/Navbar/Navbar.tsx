2c3e22d2e1a0258c57de7fe611303793925b944c
Norbert M. Soto <57424278+nsoto92@users.noreply.github.com>
2025-03-26 21:27:16 -0400

    finish nav

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,21 +4,21 @@ import styles from './Navbar.module.css';
 export default function Navbar() {
   return (
   <div className={styles.container}>
-    <Button variant="transparent">Home</Button>
-    <Divider orientation="vertical" />
-    <Button variant="transparent">CDs Reggaetón</Button>
-    <Divider orientation="vertical" />
-    <Button variant="transparent">CDs "Old School"</Button>
-    <Divider orientation="vertical" />
-    <Button variant="transparent">LON</Button>
-    <Divider orientation="vertical" />
-    <Button variant="transparent">Rap Local</Button>
-    <Divider orientation="vertical" />
-    <Button variant="transparent">Discografías</Button>
-    <Divider orientation="vertical" />
-    <Button variant="transparent">Trap</Button>
-    <Divider orientation="vertical" />
-    <Button variant="transparent">Underground</Button>
+    <Button variant="transparent" className={styles.button}>Home</Button>
+    <Divider orientation="vertical" className={styles.divider} />
+    <Button variant="transparent" className={styles.button} >CDs Reggaetón</Button>
+    <Divider orientation="vertical" className={styles.divider} />
+    <Button variant="transparent" className={styles.button}>CDs "Old School"</Button>
+    <Divider orientation="vertical" className={styles.divider} />
+    <Button variant="transparent" className={styles.button}>LON</Button>
+    <Divider orientation="vertical" className={styles.divider} />
+    <Button variant="transparent" className={styles.button}>Rap Local</Button>
+    <Divider orientation="vertical" className={styles.divider} />
+    <Button variant="transparent" className={styles.button}>Discografías</Button>
+    <Divider orientation="vertical" className={styles.divider} />
+    <Button variant="transparent" className={styles.button}>Trap</Button>
+    <Divider orientation="vertical" className={styles.divider} />
+    <Button variant="transparent" className={styles.button}>Underground</Button>
   </div>
   )
 }
