7dcd89ede6d4c42d7a1c0ccd29bafb6d9480c137
Norbert M. Soto <57424278+nsoto92@users.noreply.github.com>
2025-04-09 19:55:24 -0400

    Update homeContent.tsx

--- components/HomePage/homeContent.tsx
+++ components/HomePage/homeContent.tsx
@@ -1,46 +1,51 @@
 'use client'
 
-import { Button } from '@mantine/core';
-import Image from 'next/image';
+import { Button, Image, Container } from '@mantine/core';
 import { useRouter } from 'next/navigation';
 export default function HomeContent() {
   const router = useRouter();
   return (
-    <>
-      <Image
-        src="/lvhpTypography.png"
-        alt="LONHP"
-        width={800}
-        height={100}
-      />
-      <Button onClick={() => router.push('/home')} variant="filled" color="ocean-blue" size="compact-xl">
-        Entrar
-      </Button>
-
-      <Image
-        src="/whiteTypography4.png"
-        alt="WTG4"
-        width={200}
-        height={125}
-      />
+    <Container style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
       <Image
         src="/whiteCombo4.png"
         alt="WTG4"
-        width={200}
-        height={125}
+        maw={200}
+        h={200}
+      />
+      <Image
+        src="/lvhpTypography.png"
+        alt="LONHP"
+        fit="contain"
+        mt={30}
+        maw={600}
+        h={250}
+      />
+      <Image
+        src="/whiteTypography4.png"
+        alt="WTG4"
+        maw={200}
+        h={200}
       />
       <Image
         src="/whiteTypography2.png"
         alt="WTG4"
-        width={200}
-        height={125}
+        maw={200}
+        h={200}
       />
       <Image
         src="/whiteTypography1.png"
         alt="WTG4"
-        width={200}
-        height={125}
+        maw={200}
+        h={200}
       />
-    </>
+      <Button
+        onClick={() => router.push('/home')}
+        variant="filled"
+        color="maroon"
+        size="compact-md"
+      >
+        Entrar
+      </Button>
+    </Container>
   )
 }
